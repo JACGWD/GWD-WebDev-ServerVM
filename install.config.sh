f14b4dc9ee5638e62e6593462e558582e02a8e40
JACGWD <gwd@johnabbott.qc.ca>
2019-02-05 12:03:11 -0500

    First Commit
    Functioning but bare bones bash configuration script for setting up a LAMP web development server starting from a generic Ubuntu 18.04LTS desktop installation.

---
+++ install.config.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+sudo apt update && apt upgrade &&  apt install -y chromium-browser filezilla flameshot apache2 php mysql-server openssh-server && snap install atom gitkraken
