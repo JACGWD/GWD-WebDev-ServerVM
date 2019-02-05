91821ce8a61a3dd10279cf602419a65eebbc9c12
JACGWD <gwd@johnabbott.qc.ca>
2019-02-05 13:00:38 -0500

    Update install.config.sh
    Formatted commands to multiple lines. Added comments.

--- install.config.sh
+++ install.config.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 
 
-sudo apt update && apt upgrade &&  apt install -y chromium-browser filezilla flameshot apache2 php mysql-server openssh-server && snap install atom gitkraken
+sudo apt update && 
+apt upgrade &&  
+apt install -y chromium-browser filezilla flameshot apache2 php mysql-server openssh-server && 
+sudo snap refresh atom gitkraken && 
+sudo snap install --classic atom && 
+sudo snap install gitkraken && 
+sudo apt autoremove 
+# sudo shutdown -r now
+
+# 'Software Choices':
+# Chromium browser \(parent project of Google Chrome)
+# Filezilla \ftp client
+# Flameshot screen capture
+# Apache web server
+# PHP language
+# MySQL database server
+# SSH server \(\for sftp)
+# Atom text editor
+# GitKraken GIT manager
